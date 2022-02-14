efe4ebb3bd1383860f9fe1aca1bf1409a1bb6ede
Jonathan W <jwilliams12117@gmail.com>
2022-02-13 20:41:17 -0700

    Updated carousel to have clickable links to deployed projects

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//Define DOM target elements
+//Carousel items
+
+//Carousel description
+
+//Carousel view buttons
+
+//Update project title
+
+//Load project description
+
+//View this project function
+//- Grab the project link
+//
+
+//Listener for view this project
+
+//Listener for view all projects
