a55066094485f5a48a947416520b5c78b8eb154e
Jonathan W <jwilliams12117@gmail.com>
2022-02-13 22:16:07 -0700

    Added dynamically updated description, title, and links to carousel, styled buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ var carousel = document.getElementById("carousel");
 
 //Carousel descriptions
 var projectDescriptions = [
-    {projectNumber: 1, projectName: "Project 1", description: "Description 1"},
-    {projectNumber: 2, projectName: "Project 2", description: "Description 2"},
-    {projectNumber: 3, projectName: "Project 3", description: "Description 3"},
-    {projectNumber: 4, projectName: "Project 4", description: "Description 4"},
-    {projectNumber: 5, projectName: "Project 5", description: "Description 5"}
+    {projectNumber: 0, projectName: "Project 1", description: "Work Day Scheduler is an app that allows you to add tasks to each block of the work day, and will highlight each block of time based on the current hour."},
+    {projectNumber: 1, projectName: "Project 2", description: "The City Weather Dashboard app lets you type in a city and see its current and five-day forecast, as well as select from a list of previously-searched cities."},
+    {projectNumber: 2, projectName: "Project 3", description: "Check Yourself is a mental health app designed to give you suggestions for fun local activities based on your mood, and can store your past daily entries for review."},
+    {projectNumber: 3, projectName: "Project 4", description: "Password Generator takes your criteria such as capitalization and special characters, and generates a random password for you to use."},
+    {projectNumber: 4, projectName: "Project 5", description: "Pokemon Type Randomizer takes a random pokemon and reassignes it a new typing, letting you reimagine and draw it."}
 ]
 
 //Carousel view buttons
@@ -35,9 +35,13 @@ $("#carousel").on("slide.bs.carousel", function(event) {
     var projectLink = $(event.relatedTarget).children("a").attr("href");
 
     //Find the description of that project based on its title
-    
+    var projectDescription = projectDescriptions[targetSlide].description;
+    console.log(projectDescription);
 
     //Load the elements to the DOM for the description section
+    $("#project-title").text(projectTitle);
+    $("#project-description").text(projectDescription);
+    $("#view-project-btn").children("a").attr("href", projectLink);
 
 })
 
